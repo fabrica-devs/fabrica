53798b15a248aadbd02cda3dac7c2557fa1af1d4
GabrielDeutner <gabriel@deutner.com>
2018-01-17 10:06:28 +0100

    Multiple Changes to CSS and JS
    Style: Added showhidebutton, updated style of inner overlay, removed hardcoded dropdown positions, added alpha non supported browser
    Index: Updated dropdowns + showHide
    Styling JS: Start writing a documentation + added new animations
    SupportedBrowser: Alpha non supported browser

--- js/styling.js
+++ js/styling.js
@@ -1,17 +1,23 @@
-var topbarLeftButtonMenu;
 var texts = [['about', `<div id="about"><h1 class="headerText">Welcome! We are Fabrica</h1><div class="logocontainer"><div class="logos"><div><img src="media/turtleowoextend.png" alt="IMG"><h2>Andreas</h2><code>Project Management/Implementation of the Table Calculation</code></div><div><img src="media/Clemens.png" alt="IMG"><h2>Clemens</h2><code>Implementation of the Table Calculation</code><p>yeah can i get uuuuuuuuhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p></div><div><img src="media/Thomas.png" alt="IMG"><h2>Thomas</h2><code>Creation of the Documentation/Helpsites</code></div><div><img src="media/Gabi.png" alt="IMG"><h2>Gabriel (Gabi) Deutner</h2><code>Design and Execution of the Layout</code><p>"Heya! Welcome to our website!<br/>As written above, I am responsible for the design and execution of the layout, if you find a mistake in the layout, send me an <a href="mailto:info@deutner.space">email!</a><br/><b>Loves turtles!</b></p></div></a></div></div></div>`]];
 
+/* On Page Load */
 $(document).ready(function () {
+    /* Hardcoded Loading Animation */
     setTimeout(function () {
         $('#overlay').fadeOut(400, function () {
             $('#overlay').html("");
         });
     }, 500);
-    topbarLeftButtonMenu = $('#topbar #leftButtonMenu').html();
-    $('#innerOverlay').css('height', $(window).height() - 72);
+
     $(window).on('resize', function () {
         $('#innerOverlay').css('height', $(window).height() - 72);
     });
+
+    topbarLeftButtonMenu = $('#topbar #leftButtonMenu').html();
+    sidebarHeight = $('#sidebar').css('height');
+    topbarMargin = $('#topbar').css('margin-left');
+    topbarWidth = $('#topbar').css('width');
+    $('#innerOverlay').css('height', $(window).height() - 72);
     $('.formattingbutton').on('click', function () {
         if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
@@ -19,7 +25,10 @@ $(document).ready(function () {
             $(this).addClass('selected');
         }
     });
+
+    fixCollapsedButtons();
 });
+
 function leavingPage(url) {
     $('#overlay').html(`<h1 class="fabricaLogoText">Fabrica</h1>
                         <p>You are now leaving the editor</p>
@@ -29,7 +38,6 @@ function leavingPage(url) {
         window.location.href = url;
     }, 200);
 }
-
 
 function writeOverlay(to) {
     if (to != 'Empty') {
@@ -56,7 +64,25 @@ function changeIndicatorButton(to, from) {
     });
 }
 
+function fixCollapsedButtons() {
+    $('.collapsed').on('mouseenter', function () {
+        let buttonClass = $(this).attr('class').replace('buttoncontainer ', '').replace('button ', '').replace('collapsed ', '');
+        $(`.dropdown.${buttonClass}`).css('left', $(this).offset().left);
+        console.log($(this).offset().left);
+    });
+    $('.collapsed').on('click', function () {
+        let buttonClass = $(this).attr('class').replace('buttoncontainer ', '').replace('button ', '').replace('collapsed ', '');
+        if ($(`.dropdown.${buttonClass}`).hasClass('toggled')) {
+            $(`.dropdown.${buttonClass}`).removeClass('toggled');
+        } else {
+            $(`.dropdown.${buttonClass}`).addClass('toggled');
+        }
+    });
+}
+
 function slideUp(to, from) {
+
+    showSidebar();
 
     changeIndicatorButton(to, from);
 
@@ -71,6 +97,8 @@ function slideUp(to, from) {
                 }
             });
             $('#topbar *').fadeIn(400);
+            fixCollapsedButtons();
+            $(`.dropdown`).removeAttr('style');
         });
         writeOverlay('Empty');
     });
@@ -78,16 +106,31 @@ function slideUp(to, from) {
 
 function slideDown(to, from) {
 
-    $('#innerOverlay').css('height', $(window).height() - 72);
+    sidebarHeight = $('#sidebar').css('height');
+    topbarMargin = $('#topbar').css('margin-left');
+    topbarWidth = $('#topbar').css('width');
+
+    if (to == 'help') {
+        hideSidebar();
+        $('#innerOverlay').css('height', $(window).height() - 400);
+        $('#innerOverlay').css('position', 'absolute');
+    } else {
+        $('#innerOverlay').css('height', $(window).height() - 72);
+        $('#innerOverlay').css('position', 'relative');
+    }
 
     writeOverlay(to);
 
     changeIndicatorButton(to, from);
 
     $('#innerOverlay').slideDown(400, function () {
-        $('#topbar #leftButtonMenu .buttoncontainer').fadeOut(400, function () {
+        $('#topbar #leftButtonMenu .buttoncontainer, .helpbuttoncontainer').fadeOut(400, function () {
             $('#topbar #leftButtonMenu .buttoncontainer').remove();
-            $('#topbar #leftButtonMenu').html(`<div class="button buttoncontainer back" onclick="slide(this, '${from}', '${to}');">Back</div>`);
+            if (to == 'help') {
+                $('#topbar #leftButtonMenu').append(topbarLeftButtonMenu);
+                fixCollapsedButtons();
+            }
+            $('#topbar #leftButtonMenu').prepend(`<div class="button buttoncontainer back" onclick="slide(this, '${from}', '${to}');">Back</div>`);
             $('#topbar #leftButtonMenu .buttoncontainer').fadeIn(400);
             $('#topbar #leftButtonMenu .buttoncontainer').css('display', 'flex', 'align-items', 'center', 'text-align', 'center');
         });
@@ -101,6 +144,50 @@ function slide(button, to, from) {
         slideDown(to, from);
     } else {
         slideUp(to, from);
-
     }
+}
+
+function hideSidebar(special) {
+    $('#sidebar, #sidebarshadowfix').animate({
+        height: 0
+    }, 400, function () {
+        $('#leftButtonMenu').animate({
+            margin: '0 0 0 0.4%'
+        }, 200);
+        $('#topbar').animate({
+            margin: 0,
+            width: '100%'
+        }, 400);
+    });
+    if (special == "empty") {
+        $('#leftButtonMenu').prepend(`<p class="showHideButton" onclick="showSidebar('empty')">Show</p>`);
+        $('#leftButtonMenu .showHideButton').animate({
+            height: '20px'
+        }, 400);
+    }
+}
+
+function showSidebar(special) {
+    if (special == "empty") {
+        $('#leftButtonMenu .showHideButton').animate({
+            width: 0
+        }, 400, function () {
+            $('#leftButtonMenu .showHideButton').remove();
+        });
+    }
+    $('#topbar').animate({
+        margin: `0 0 0 ${topbarMargin}`,
+        width: topbarWidth
+    }, 400, function () {
+        $('#sidebar, #sidebarshadowfix').animate({
+            height: sidebarHeight
+        }, 400);
+    });
+    $('#leftButtonMenu').animate({
+        margin: '0 0 0 0'
+    }, 200);
+}
+
+function showFunctionBar() {
+    $('#topbar #leftButtonMenu').append(topbarLeftButtonMenu).fadeIn(400);
 }
