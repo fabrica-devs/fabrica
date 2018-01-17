4bad77fe65e90b3c2a18ce86b64fd7074212ab4b
Clemens KERN <ker17529@spengergasse.at>
2018-01-17 12:13:57 +0100

    added use of localStorage, maybe 'optimized' url usage

--- js/table.js
+++ js/table.js
@@ -1,4 +1,14 @@
-function urlEdit( newPage, sideBarHide ) {
+/* Clemens */
+
+window.onload = function() {
+    if ( typeof(Storage) !== undefined ) {
+        if ( localStorage.getItem("lastSite").split("?p=").length > 1 && localStorage.getItem("lastSite") !== undefined) {
+            urlEdit(localStorage.getItem("lastSite").split("?p=")[1]);
+        }
+    }
+}
+
+function urlEdit( newPage ) {
     switch(newPage) {
         case "about":
             document.getElementById("bottomInfo").children[0].children[0].click();
@@ -9,9 +19,9 @@ function urlEdit( newPage, sideBarHide ) {
             urlChange(newPage);
             break;
         default:
-            var curUrl = window.location.href;
-            if ( curUrl.split("?b=").length > 1 ) {
-                
+            window.location.href = window.location.href.split("?p=")[0];
+            if ( typeof(Storage) !== undefined ) {
+                localStorage.setItem("lastSite", window.location.href);
             }
             break;
     }
@@ -20,11 +30,16 @@ function urlEdit( newPage, sideBarHide ) {
 function urlChange(newPage) {
     var curUrl = window.location.href;
     if ( curUrl.split("?p=").length > 1 ) {
-        curUrl.split("?p=")[1] = "about";
+        curUrl = curUrl.split("?p=")[0] + "?p=" + newPage;
     } else {
         curUrl += "?p=" + newPage;
     }
+    if ( typeof(Storage) !== undefined ) {
+        localStorage.setItem("lastSite", curUrl);
+    }
 }
+
+/* Andi */
 
 $(document).ready(() => {
     const container = document.getElementById('table')
