6528c2c5b084e4e7aca3f4300c86c548043d9317
Andreas Hässler <andreas.haessler@outlook.at>
2018-01-27 01:02:16 +0100

    Move source files to src folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,18 @@ const CopyPlugin = require('copy-webpack-plugin')
 const CleanPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './js/index.js',
+    entry: './src/js/index.js',
     devtool: 'source-map',
     output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     plugins: [
         new CopyPlugin([
-            { from: 'media', to: 'media' },
-            { from: '3rdparty', to: '3rdparty'},
-            { from: 'index.html', to: 'index.html' },
-            { from: 'css/*.css', to: '' }
+            { from: 'src/media', to: 'media' },
+            { from: 'src/3rdparty', to: '3rdparty'},
+            { from: 'src/index.html', to: 'index.html' },
+            { from: 'src/css/*.css', to: 'css', flatten: true }
         ]),
         new CleanPlugin(['dist'])
     ]
